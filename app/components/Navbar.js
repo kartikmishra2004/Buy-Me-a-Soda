434e5072a6efb2acccfad6ae57458ef8d3de8791
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-09-18 16:12:40 +0530

    Hero section in home component

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,47 +1,64 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import logo from "@/app/images/logo.svg";
+import Link from "next/link";
 
 const Navbar = () => {
+    const [navbar, setNavbar] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 20) {
+                setNavbar(true);
+            } else {
+                setNavbar(false);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="bg-[#111827] z-10">
+        <nav className={`${navbar ? 'bg-[#111827]' : 'bg-[#11182700]'} z-10 fixed w-full`}>
             <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 items-center justify-between">
+                <div className="flex h-[4.5rem] items-center justify-between">
                     <div className="flex-1 md:flex md:items-center md:gap-12">
                         <a className="flex text-lg font-bold gap-1 text-gray-300" href="/">
-                            <Image className="md:w-36 w-[7rem]" priority={true} src={logo} alt="logo" ></Image>
+                            <Image className="md:w-36 w-[7rem]" priority={true} src={logo} alt="logo" />
                         </a>
                     </div>
                     <div className="md:flex md:items-center md:gap-12">
                         <nav aria-label="Global" className="hidden md:block">
                             <ul className="flex items-center gap-6 text-sm">
                                 <li>
-                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="#"> {" "}About{" "}</a>
+                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="/">About</a>
                                 </li>
                                 <li>
-                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="#"> {" "}Careers{" "}</a>
+                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="/">Careers</a>
                                 </li>
                                 <li>
-                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="#"> {" "}History{" "}</a>
+                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="/">History</a>
                                 </li>
                                 <li>
-                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="#"> {" "}Services{" "}</a>
+                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="/">Services</a>
                                 </li>
                                 <li>
-                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="#"> {" "}Projects{" "}</a>
+                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="/">Projects</a>
                                 </li>
                                 <li>
-                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="#"> {" "}Blog{" "}</a>
+                                    <a className="text-gray-400 transition hover:text-gray-50/75" href="/">Blog</a>
                                 </li>
                             </ul>
                         </nav>
                         <div className="flex items-center gap-4">
                             <div className="sm:flex sm:gap-4">
-                                <a className="rounded-md bg-blue-800 px-5 py-2.5 text-sm font-medium text-gray-300 shadow" href="#">Login</a>
+                                <a className="text-white cursor-pointer bg-gradient-to-r from-blue-700 via-blue-800 to-blue-900 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">Login</a>
                                 <div className="hidden sm:flex">
-                                    <a className="rounded-md bg-[#333333] px-5 py-2.5 text-sm font-medium text-gray-300" href="#">
-                                        Register
+                                    <a className="rounded-lg bg-gray-300 px-5 py-2.5 text-sm font-medium text-gray-700" href="/">
+                                        Signup
                                     </a>
                                 </div>
                             </div>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,23 @@
+"use client";
+import Image from "next/image";
+import soda from "@/app/images/soda.gif";
+
 export default function Home() {
   return (
-    <div class="h-screen w-full bg-slate-950">
-      <div class="h-screen w-full bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:40px_70px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
-
+    <>
+      <div className="w-full md:px-0 px-5 h-[75vh] flex flex-col justify-center items-center text-gray-300 gap-2">
+        <div className="flex justify-center items-center">
+          <h1 className="md:text-5xl text-2xl font-bold">Buy Me a Soda</h1>
+          <span>
+            <Image className="md:w-14 w-12" alt="" src={soda} priority={true}></Image>
+          </span>
+        </div>
+        <p className="md:text-base text-sm text-center md:text-left">A Crowdfunding Platform Where Every Sip Counts – Help Innovators and Dreamers Turn Their Ideas Into Reality.</p>
+        <div className="buttons mt-4">
+          <button type="button" className="text-white bg-gradient-to-r from-blue-700 via-blue-800 to-blue-900 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Get started</button>
+          <button type="button" className="text-gray-700 bg-gradient-to-r bg-gray-300 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read more</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
