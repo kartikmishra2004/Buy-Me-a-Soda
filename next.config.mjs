3904db141db86560739e7b9039ae4375ddc2ce8c
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-09-28 16:03:01 +0530

    Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'avatars.githubusercontent.com',
-            port: '',
-        },
-        {
-            protocol: 'https',
-            hostname: 'lh3.googleusercontent.com',
-            port: '',
-        },
-        {
-            protocol: 'https',
-            hostname: 'plus.unsplash.com',
-            port: '',
-        },
-        {
-            protocol: 'https',
-            hostname: 'encrypted-tbn0.gstatic.com',
-            port: '',
-        },
-        {
-            protocol: 'https',
-            hostname: 'www.svgrepo.com',
-            port: '',
-        },
-        {
-            protocol: 'https',
-            hostname: 'res.cloudinary.com',
-            port: '',
-        },
-    ],
-},};
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'plus.unsplash.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'encrypted-tbn0.gstatic.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.svgrepo.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
