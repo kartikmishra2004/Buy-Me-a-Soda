53ad1f71ebc4f4460dacbad5947aa0ce80707f8c
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-09-28 16:26:53 +0530

    error fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,38 @@
 const nextConfig = {
     images: {
         unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'plus.unsplash.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'encrypted-tbn0.gstatic.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.svgrepo.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+            },
+        ],
     },
 };
 
